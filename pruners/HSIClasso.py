5efbea89ad8050c38e596e7b21de86760d5a9281
GuoSong <1424956666@qq.com>
2022-11-29 12:17:04 +0800

    bug fix

--- pruners/HSIClasso.py
+++ pruners/HSIClasso.py
@@ -7,61 +7,49 @@ import torch
 import torch.nn as nn
 import os
 import logging
+import time
 #import cuml
-os.environ['CUDA_VISIBLE_DEVICES']='1'
-device = torch.device(f"cuda:{1}") if torch.cuda.is_available() else 'cpu'
+os.environ['CUDA_VISIBLE_DEVICES']='2'
+device = torch.device(f"cuda:{2}") if torch.cuda.is_available() else 'cpu'
 from sklearn.metrics.pairwise import rbf_kernel
 def centering(K):
     if len(K.shape)==3:
         d = K.shape[0]
         n = K.shape[1]
-        I=torch.randn((d,n,n))
-        I[:]=torch.eye(n)
-        unit = torch.ones((d,n,n))
-        H = I - unit / n
-        return torch.matmul(torch.matmul(H,K),H)
+        I = np.tile(np.eye(n), (d, 1, 1))
+        H = I - np.full((d,n,n), 1/n) #unit / n
+        return H@K@H
     else :
         n=K.shape[0]
-        I=torch.eye(n)
-        unit=torch.ones((n,n))
-        H = I - unit / n
-        return torch.matmul(torch.matmul(H,K),H)
+        I = np.eye(n)
+        H = I - np.full((n,n), 1/n)#unit / n
+        return H@K@H
 
-def HSIC_lasso_pruning(X, Y, W, alpha=1e-3, threshold=1,debug=False):
-    # Example: B is sample number
-    #        : c_in is channel input
-    #        : c_out is channel output
-    #        : 3x3 is kernel size
-    # X shape: [B, c_in, 3, 3]
-    # Y shape: [B, c_out]
-    # W shape: [c_out, c_in, 3, 3]
-    # Linear
-    # X shape: [B, c_in]
-    # Y shape: [B, c_out]
-    # W shape: [c_out, c_in]
-    ##求HSIC X(B,c_in,h*w)  Y(B,c_out*h*w)
+def HSIC_lasso_pruning(X, Y, W, alpha=1e-6, threshold=1,debug=False):
     b, c, l = X.shape
+
     X=X.transpose(1,0,2)  #(c_in,B,h*w)
-    K=None
-    X=torch.from_numpy(X)#.to(device)
-    #K=X.matmul(torch.from_numpy(X.numpy().transpose(0,2,1)))
-    tmp = X.unsqueeze(2) - X.unsqueeze(1) # (c_in, B,B, h*w)
+    K=None      
+    #K=X.matmul(torch.from_numpy(X.numpy().transpose(0,2,1)))#linear kernel
+    G = X@X.transpose(0,2,1)
+    H = np.tile(np.diagonal(G, axis1=1,axis2=2)[:,None,:], (1,b, 1))
     gamma = 1.0 / X.shape[-1]
-    K = torch.exp(- (tmp ** 2).sum(dim=-1) * gamma) #(c_in,N,N)
+    K = np.exp((H + H.transpose(0,2,1) -2*G)*(-gamma))
+    
     K_ba=centering(K) #(c_in,N,N)
     K_ba=K_ba.reshape(c,-1)
-    K_ba=K_ba.transpose(1,0).numpy()# (N*N,c_in)
+    K_ba = K_ba.T
 
-        
-
-    Y=torch.from_numpy(Y)
-    tmp=Y.unsqueeze(1) - Y.unsqueeze(0)
+    
+    G = Y@Y.T
+    H = np.tile(np.diagonal(G), (b, 1))
     gamma = 1.0 / Y.shape[-1]
-    L = torch.exp(- (tmp ** 2).sum(dim=-1) * gamma)
+    L = np.exp((H + H.T -2*G)*(-gamma))  
     L_ba=centering(L)
-    L_ba=L_ba.reshape(-1).numpy()#N*N
+    L_ba=L_ba.reshape(-1)#.numpy()#N*N
 
     solver = Lasso(alpha=alpha, warm_start=True, selection='random', random_state=0)
+
     def solve(alpha):
         """ Solve the Lasso"""
         solver.alpha = alpha
@@ -72,14 +60,12 @@ def HSIC_lasso_pruning(X, Y, W, alpha=1e-3, threshold=1,debug=False):
 
 
 
-    left = 0  
+    left = 0  # minimum alpha is 0, which means don't use lasso regularizer at all
     right = alpha
-    #while True:
     keep_inds, keep_num, coef = solve(right)
-
-    if keep_num<threshold:
-        lbound=threshold
-        rbound=threshold
+    if keep_num < threshold:
+        lbound=threshold   # threshold
+        rbound=threshold   # threshold
         step=0
         while True:
             step+=1
@@ -87,7 +73,6 @@ def HSIC_lasso_pruning(X, Y, W, alpha=1e-3, threshold=1,debug=False):
             alpha = (left + right) / 2
             keep_inds, keep_num, coef = solve(alpha)
             # print loss
-            # product has size [B x c_out, c_in]
             loss = 1 / (2 * float(X.shape[0])) * \
                 np.sqrt(np.sum((L_ba - np.matmul(K_ba, coef)) ** 2, axis=0)) + \
                 alpha * np.sum(np.fabs(coef))
@@ -104,7 +89,7 @@ def HSIC_lasso_pruning(X, Y, W, alpha=1e-3, threshold=1,debug=False):
             else:
                 break
 
-            if alpha < 1e-15:
+            if alpha < 1e-20:
                 break
             if step>50:
                 break
